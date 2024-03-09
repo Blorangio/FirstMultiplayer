f1d8f094279e9cf60503fee02018bcc6cdd2d455
Blorangio <fadpratt@gmail.com>
2024-03-08 22:09:45 -0600

    Collision Between Two Polygons In

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 let camera = new Camera(new Point(0, 0));
 let player = new Player(genRect(100, 100, 50, 50));
 
+let box = new Shape([new Point(0, 0), new Point(30, 50), new Point(50, 30)]);
+
+let immovableCollisions = [box];
+
 let SPEED = 3;
 
 function gameLoop() {
     clearCanvas();
     move(player);
+
+    for(let i in immovableCollisions) {
+        let current = immovableCollisions[i];
+        let collisionUpdate = polygonCollision(current, player);
+        if(collisionUpdate.isColliding) {
+            player.translate(collisionUpdate.depth * collisionUpdate.normal.x, collisionUpdate.depth * collisionUpdate.normal.y);
+            camera.position.x = player.getPosition().x - 500;
+            camera.position.y = player.getPosition().y - 562/2;
+        }
+    }
+
     paint.fillStyle = "orange";
     drawShape(player);
+    paint.fillStyle = "black";
+    drawShape(box);
 }
 
 loop();
